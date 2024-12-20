2373380ec4ceb32713ef728507b881b02164f55b
Binee <ckqlss@gmail.com>
2024-12-20 21:44:42 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
-import { Button } from "@/components/ui/button";
-import { createClient } from "@/supabase/server";
-import Image from "next/image";
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { supabase } from "@/supabase/client";
 
-export default async function Home() {
-  const supabase = createClient();
-
-  const { data } = await supabase.from("wine").select("*");
+export default function Home() {
+  const { data } = useQuery({
+    queryKey: ["wines"],
+    queryFn: async () => {
+      const { data } = await supabase.from("wine").select("*");
+      return data;
+    },
+  });
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
